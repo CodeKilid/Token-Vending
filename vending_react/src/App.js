620aa884dccc329fba50f284376997f4f538529a
alireza alizadeh <alireza.aa9700@gmail.com>
2023-06-07 11:55:29 +0330

    the node addresses is the same

--- vending_react/src/App.js
+++ vending_react/src/App.js
@@ -2,8 +2,7 @@ import "./App.css"
 import { ethers } from "ethers"
 import VendingMachine from "./artifacts/contracts/VendingMachine.sol/VendingMachine.json"
 
-const VENDING_MACHINE_ADDRESS_seyyed = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
-const VENDING_MACHINE_ADDRESS_cap = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
+const VENDING_MACHINE_ADDRESS = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
 
 function App() {
     return <div className="App"></div>
