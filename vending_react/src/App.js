5c45cb3e98f6f3c2a53384f6611c498d4785e805
alireza alizadeh <alireza.aa9700@gmail.com>
2023-06-07 12:00:34 +0330

    set the abstract functions

--- vending_react/src/App.js
+++ vending_react/src/App.js
@@ -5,6 +5,23 @@ import VendingMachine from "./artifacts/contracts/VendingMachine.sol/VendingMach
 const VENDING_MACHINE_ADDRESS = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
 
 function App() {
+    /* functions */
+    // get request account
+    async function getRequestAccount() {}
+
+    // get buyer balance
+    async function getBuyerBalance() {}
+
+    // get vending balance
+    async function getVendingBalance() {}
+
+    // purchase
+    async function purchase() {}
+
+    // restock
+    async function restock() {}
+
+    // front side ---> jsx
     return <div className="App"></div>
 }
 
