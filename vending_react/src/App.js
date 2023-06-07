2dc59588609390dd95ce2577972df88b518e7d30
ElahiAli <ali_elahi00@outlook.com>
2023-06-07 12:28:35 +0330

    resolving

--- vending_react/src/App.js
+++ vending_react/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css"
 import { ethers } from "ethers"
+import { useState } from "react"
 import VendingMachine from "./artifacts/contracts/VendingMachine.sol/VendingMachine.json"
 
 const VENDING_MACHINE_ADDRESS = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
@@ -7,12 +8,29 @@ const VENDING_MACHINE_ADDRESS = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
 function App() {
     /* functions */
     // get request account
+    const [buyerBalance, setBuyerBalance] = useState("")
+    const [vendingBalance, setVendingBalance] = useState("")
+
     async function RequestAccount() {
         await window.ethereum.request({ method: "eth_requestAccounts" })
     }
 
     // get buyer balance
-    async function getBuyerBalance() {}
+    async function getBuyerBalance() {
+        if (typeof window.ethereum !== "undefined") {
+            // await RequestAccount()
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const signer = provider.getSigner()
+
+            const vendingContract = new ethers.Contract(
+                VENDING_MACHINE_ADDRESS,
+                VendingMachine.abi,
+                provider
+            )
+            const transaction = vendingContract.getBuyerBalancer()
+        }
+    }
 
     // get vending balance
     async function getVendingBalance() {
