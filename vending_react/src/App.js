03a6d1f387d348913e9b7fc5b747b40f8e75ef27
alireza alizadeh <alireza.aa9700@gmail.com>
2023-06-07 12:26:00 +0330

    getVendingBalance

--- vending_react/src/App.js
+++ vending_react/src/App.js
@@ -7,13 +7,35 @@ const VENDING_MACHINE_ADDRESS = "0x687bB6c57915aa2529EfC7D2a26668855e022fAE"
 function App() {
     /* functions */
     // get request account
-    async function getRequestAccount() {}
+    async function RequestAccount() {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+    }
 
     // get buyer balance
     async function getBuyerBalance() {}
 
     // get vending balance
-    async function getVendingBalance() {}
+    async function getVendingBalance() {
+        if (typeof window.ethereum !== "undefined") {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const contract = new ethers.Contract(
+                VENDING_MACHINE_ADDRESS,
+                VendingMachine.abi,
+                provider
+            )
+        }
+        // call contract.getVendingBalance() and display current balance of vending machine
+        /* function getVendingMachineBalance() public view returns (uint256) {
+              return donutBalances[address(this)];
+        }*/
+        try {
+            const vendingBalance = await contract.getVendingMachineBalance()
+            console.log("vending balance is: ", vendingBalance)
+            // set in the useState
+        } catch (error) {
+            console.log("Error: ", error)
+        }
+    }
 
     // purchase
     async function purchase() {}
