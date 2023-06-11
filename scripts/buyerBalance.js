d4d64be7cdf65b1f4a4eafdbeea2946e32b40e3c
ElahiAli <ali_elahi00@outlook.com>
2023-06-11 11:29:03 +0330

    scripts for interacting with contracct

--- scripts/buyerBalance.js
+++ scripts/buyerBalance.js
@@ -4,14 +4,6 @@ async function main() {
     const user = (await getNamedAccounts()).user
     const vendingContract = await ethers.getContract("VendingMachine", user)
 
-    console.log("purchasing...")
-    const fee = await vendingContract.price()
-    const purchaseCount = 2
-    const purchase = await vendingContract.purchase(purchaseCount, { value: purchaseCount * fee })
-    await purchase.wait(1)
-
-    console.log("purchase is done.")
-
     const buyerBalance = await vendingContract.getBuyerBalancer()
     console.log(`buyer balance is: ${buyerBalance}`)
 }
