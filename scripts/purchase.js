565da262d9a1a359c28cd72c8e278dbe6713def7
ElahiAli <ali_elahi00@outlook.com>
2023-06-11 14:45:03 +0330

    resolve the signer problem

--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,12 +1,19 @@
-const { getNamedAccounts, ethers } = require("hardhat")
+const { ethers } = require("hardhat")
+const { abi } = require("../artifacts/contracts/VendingMachine.sol/VendingMachine.json")
+
+require("dotenv").config()
 
 async function main() {
-    // const user = (await getNamedAccounts()).user
-    // console.log(user)
-    const vendingContract = await ethers.getContract("VendingMachine")
+    const contractAddress = "0x66e5CE39C90341f54b6D5B43710B2e034CDe6A7D"
+    const provider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_RPC_URL)
+    const singer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const vendingContract = new ethers.Contract(contractAddress, abi, singer)
 
     console.log("purchasing...")
+
     const fee = await vendingContract.price()
+    console.log(`price is: ${fee}`)
+
     const purchaseCount = 2
     const purchase = await vendingContract.purchase(purchaseCount, { value: purchaseCount * fee })
     await purchase.wait(1)
